3c950b225c597ddcba152fc26b2ea8a4a6699a65
Sergey (css4) <sergei.blade14@gmail.com>
2019-07-31 08:25:41 +0300

    move from scss to styled-components fro header, homepage, custom-button, collection-item component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
 import { connect } from 'react-redux'
-import {createStructuredSelector} from 'reselect'
+import { createStructuredSelector } from 'reselect'
 
-import './header.styles.scss'
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink
+} from './header.styles'
+
 import CardIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { selectCurrentUser } from '../../redux/user/user.selectors'
@@ -13,26 +18,25 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors'
 
 const Header = ({ currentUser, hidden }) => {
   return (
-    <div className="header">
-      <Link to="/" className="logo-container">
-        <Logo className="logo"/>
-      </Link>
-      <div className="options">
-        <Link className="option" to="/shop">SHOP</Link>
-        <Link className="option" to="/contact">CONTACT</Link>
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo/>
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/contact">CONTACT</OptionLink>
         {
           currentUser ?
-            <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+            <OptionLink as="div" onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
             :
-            <Link className="option" to="/signin">SIGN IN</Link>
+            <OptionLink to="/signin">SIGN IN</OptionLink>
         }
         <CardIcon/>
-      </div>
+      </OptionsContainer>
       {
         hidden ? null : <CartDropdown/>
       }
-
-    </div>
+    </HeaderContainer>
   )
 }
 
