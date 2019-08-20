af3752c8c2f75d71eff03d9aa844fd0875870946
Sergey (css4) <sergei.blade14@gmail.com>
2019-08-20 16:26:28 +0300

    rewrited to grahpql all redux components

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,69 +1,144 @@
-import { gql } from 'apollo-boost'
+import { gql } from 'apollo-boost';
 
-import { addItemToCart, getCartItemCount } from './cart.utils'
+import {
+  addItemToCart,
+  removeItemFromCart,
+  clearItemFromCart,
+  getCartTotal,
+  getCartItemCount
+} from './cart.utils';
 
 export const typeDefs = gql`
-    extend type Item {
-        quantity: Int
-    }
-    
-    extend type Mutation {
-        ToggleCartHidden: Boolean!
-        AddItemToCart(item: Item!): [Item]
-    }
-`
+  extend type Item {
+    quantity: Int
+  }
+
+  extend type DateTime {
+    nanoseconds: Int!
+    seconds: Int!
+  }
+
+  extend type User {
+    id: ID!
+    displayName: String!
+    email: String!
+    createdAt: DateTime!
+  }
+
+  extend type Mutation {
+    ToggleCartHidden: Boolean!
+    AddItemToCart(item: Item!): [Item]!
+    SetCurrentUser(user: User!): User!
+    RemoveItemFromCart(item: Item!): [Item]!
+    ClearItemFromCart(item: Item!): [Item]!
+  }
+`;
 
 const GET_CART_HIDDEN = gql`
-    {
-        cartHidden @client
-    }
-`
-
-const GET_CART_ITEMS = gql`
-    {
-        cartItems @client
-    }
-`
+  {
+    cartHidden @client
+  }
+`;
 
 const GET_ITEM_COUNT = gql`
-    {
-        itemCount @client
-    }
-`
+  {
+    itemCount @client
+  }
+`;
+
+const GET_CART_TOTAL = gql`
+  {
+    cartTotal @client
+  }
+`;
+
+const GET_CART_ITEMS = gql`
+  {
+    cartItems @client
+  }
+`;
+
+const GET_CURRENT_USER = gql`
+  {
+    currentUser @client
+  }
+`;
+
+const updateCartItemsRelatedQueries = (cache, newCartItems) => {
+  cache.writeQuery({
+    query: GET_ITEM_COUNT,
+    data: { itemCount: getCartItemCount(newCartItems) }
+  });
+
+  cache.writeQuery({
+    query: GET_CART_TOTAL,
+    data: { cartTotal: getCartTotal(newCartItems) }
+  });
+
+  cache.writeQuery({
+    query: GET_CART_ITEMS,
+    data: { cartItems: newCartItems }
+  });
+};
 
 export const resolvers = {
   Mutation: {
     toggleCartHidden: (_root, _args, { cache }) => {
-
-      const {cartHidden} = cache.readQuery({
+      const { cartHidden } = cache.readQuery({
         query: GET_CART_HIDDEN
-      })
+      });
 
       cache.writeQuery({
         query: GET_CART_HIDDEN,
-        data: {cartHidden: !cartHidden}
-      })
+        data: { cartHidden: !cartHidden }
+      });
 
-      return !cartHidden
+      return !cartHidden;
     },
-    addItemToCart:(_root, {item}, {cache}) => {
-      const {cartItems} = cache.readQuery({
+
+    addItemToCart: (_root, { item }, { cache }) => {
+      const { cartItems } = cache.readQuery({
         query: GET_CART_ITEMS
-      })
+      });
 
-      const newCartItems = addItemToCart(cartItems, item)
+      const newCartItems = addItemToCart(cartItems, item);
 
+      updateCartItemsRelatedQueries(cache, newCartItems);
+
+      return newCartItems;
+    },
+
+    removeItemFromCart: (_root, { item }, { cache }) => {
+      const { cartItems } = cache.readQuery({
+        query: GET_CART_ITEMS
+      });
+
+      const newCartItems = removeItemFromCart(cartItems, item);
+
+      updateCartItemsRelatedQueries(cache, newCartItems);
+
+      return newCartItems;
+    },
+
+    clearItemFromCart: (_root, { item }, { cache }) => {
+      const { cartItems } = cache.readQuery({
+        query: GET_CART_ITEMS
+      });
+
+      const newCartItems = clearItemFromCart(cartItems, item);
+
+      updateCartItemsRelatedQueries(cache, newCartItems);
+
+      return newCartItems;
+    },
+
+    setCurrentUser: (_root, { user }, { cache }) => {
       cache.writeQuery({
-        query: GET_ITEM_COUNT,
-        data: {itemCount: getCartItemCount(newCartItems)}
-      })
+        query: GET_CURRENT_USER,
+        data: { currentUser: user }
+      });
 
-      cache.writeQuery({
-        query: GET_CART_ITEMS,
-        data: {cartItems: newCartItems}
-      })
-
-      return newCartItems
+      return user;
     }
   }
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,49 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks'
 
-import { ApolloProvider } from 'react-apollo'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloClient } from 'apollo-boost'
 
-import { store, persistor, } from './redux/store'
-
-import './index.css'
-import App from './App'
-import {resolvers, typeDefs} from './graphql/resolvers'
+import './index.css';
+import { default as App } from './App/App.container';
+import { resolvers, typeDefs } from './graphql/resolvers';
+import { default as data } from './graphql/initial-data';
 
 const httpLink = createHttpLink({
   uri: 'https://crwn-clothing.com'
-})
+});
 
-const cache = new InMemoryCache()
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
   cache,
   typeDefs,
   resolvers
-})
+});
 
-client.writeData({
-  data: {
-    cartHidden: true,
-    cartItems: [],
-    itemCount: 0
-  }
-})
+client.writeData({ data });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
+    <ApolloHooksProvider client={client}>
       <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App/>
-        </PersistGate>
+          <App />
       </BrowserRouter>
-    </Provider>
-  </ApolloProvider>
-  , document.getElementById('root'))
+    </ApolloHooksProvider>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
